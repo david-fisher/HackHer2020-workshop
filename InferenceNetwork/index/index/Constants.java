ecb3c9941c0d4dc1ce259f035b9c5bbd079a2e5f
David Fisher <dfisher@cs.umass.edu>
2019-12-22 10:00:57 -0500

    refactor hardcoded filenames to constants.

---
+++ InferenceNetwork/index/index/Constants.java
@@ -0,0 +1,11 @@
+package index;
+
+public class Constants {
+
+	// Index file names.
+	static final String LOOKUP_TXT = "lookup.txt";
+    static final String DOC_LENGTH_TXT = "docLength.txt";
+	static final String PLAY_IDS_TXT = "playIds.txt";
+	static final String SCENE_ID_TXT = "sceneId.txt";
+	static final String INVLIST = "invlist";
+}
